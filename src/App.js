233780f71b9655965082c7b415f2a31ab0913026
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-06-04 16:36:58 -0400

    displays cat to screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Cat from './components/cat/Cat';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Cat />
     </div>
   );
 }
