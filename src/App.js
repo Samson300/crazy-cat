07ffcd40f9111b1598255641b8bbe4a425721b8a
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-06-04 16:47:14 -0400

    adds map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Cat from './components/cat/Cat';
+import Map from './components/map/Map';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Map />
       <Cat />
     </div>
   );
